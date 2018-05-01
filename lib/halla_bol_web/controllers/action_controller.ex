8c62b6a597b9cf373df65f65f354d154893714a6
Hiemanshu Sharma <hiemanshu@gmail.com>
2018-05-02 00:08:57 +0300

    Actions: Get campaign ID from URL

--- lib/halla_bol_web/controllers/action_controller.ex
+++ lib/halla_bol_web/controllers/action_controller.ex
@@ -8,12 +8,17 @@ defmodule HallaBolWeb.ActionController do
 
   action_fallback HallaBolWeb.FallbackController
 
-  def index(conn, _params, _) do
+  def index(conn, _params) do
     actions = Actions.list_actions()
     render(conn, "index.json", actions: actions)
   end
 
-  def create(conn, %{"action" => action_params}) do
+  def create(conn, %{"action" => action_params, "campaign_slug" => campaign_slug}) do
+    campaign = Campaigns.get_campaign!(campaign_slug)
+
+    action_params = action_params
+    |> Map.put("campaign_id", campaign.id)
+
     with {:ok, %Action{} = action} <- Actions.create_action(action_params) do
       conn
       |> put_status(:created)
@@ -22,20 +27,29 @@ defmodule HallaBolWeb.ActionController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"id" => id, "campaign_slug" => campaign_slug}) do
+    Campaigns.get_campaign!(campaign_slug)
+
     action = Actions.get_action!(id)
     render(conn, "show.json", action: action)
   end
 
-  def update(conn, %{"id" => id, "action" => action_params}, _) do
+  def update(conn, %{"id" => id, "action" => action_params, "campaign_slug" => campaign_slug}, _) do
+    campaign = Campaigns.get_campaign!(campaign_slug)
+
     action = Actions.get_action!(id)
+
+    action_params = action_params
+    |> Map.put("campaign_id", campaign.id)
 
     with {:ok, %Action{} = action} <- Actions.update_action(action, action_params) do
       render(conn, "show.json", action: action)
     end
   end
 
-  def delete(conn, %{"id" => id}, _) do
+  def delete(conn, %{"id" => id, "campaign_slug" => campaign_slug}, _) do
+    Campaigns.get_campaign!(campaign_slug)
+
     action = Actions.get_action!(id)
     with {:ok, %Action{}} <- Actions.delete_action(action) do
       send_resp(conn, :no_content, "")
